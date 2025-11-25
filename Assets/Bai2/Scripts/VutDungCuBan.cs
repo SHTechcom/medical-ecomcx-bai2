9ce1d5e6ea250799eac637b01de50e8d7c4716a0
huytq <tranquanghuy12110@gmail.com>
2025-11-26 02:56:04 +0700

    fix and update

---
+++ Assets/Bai2/Scripts/VutDungCuBan.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VutDungCuBan : MonoBehaviour
+{
+    public Transform bang;
+    Vector3 target = new Vector3(0.583309412f, 0.087f, 1.35699999f);
+    public UnityEvent OnCompleted;
+
+    public void Play()
+    {
+        bang.DOLocalMove(target, 1).OnComplete(() =>
+        {
+            OnCompleted?.Invoke();
+        });
+    }
+}
